ad4cd0f62e7551a0f7a5b5a45722863a6aefe64e
mike zhang <zhangmingkai.1989@gmail.com>
2020-12-16 22:34:04 +0800

    update packet process

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,39 @@
+extern crate byteorder;
 extern crate failure;
+#[macro_use]
 extern crate futures;
 extern crate tokio;
 
 use failure::{Error, ResultExt};
-use tokio::prelude::*;
 use std::net::SocketAddr;
 use tokio::net::TcpStream;
+use tokio::prelude::*;
 
+pub struct Zookeeper {}
 
-pub struct Zookeeper{}
-
-impl Zookeeper{
-    fn connect(addr: &SocketAddr) -> impl Future<Item=Self, Error=failure::Error>{
-        tokio::net::TcpStream::connect(addr).and_then(|stream|{
-            Self::handshake(stream)
-        })
+impl Zookeeper {
+    fn connect(addr: &SocketAddr) -> impl Future<Item = Self, Error = failure::Error> {
+        tokio::net::TcpStream::connect(addr).and_then(|stream| Self::handshake(stream))
     }
 
-    async fn handshake(stream: tokio::net::TcpStream) ->impl Future<Item=Self, Error=failure::Error>{
-        let request = proto::Connection{};
-        let mut stream = proto::wrap(stream);
-        stream.send(request).and_then(|stream|{
-            stream.receive()
-        }).and_then(|(response, stream)|{
-            Zookeeper{}
-        })
+    async fn handshake(
+        stream: tokio::net::TcpStream,
+    ) -> impl Future<Item = Self, Error = failure::Error> {
+        let request = proto::Connection {};
+        let mut stream = proto::Packetizer::new(stream);
+        stream
+            .send(request)
+            .and_then(|stream| stream.receive())
+            .and_then(|(response, stream)| Zookeeper {})
     }
 }
 
 mod proto;
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn connect(){
-        let zk = tokio::run(Zookeeper::connect())
-    }
+    fn connect() {}
 }
-
--- src/proto.rs
+++ src/proto.rs
@@ -1,57 +1,143 @@
+use std::io;
+
+use byteorder::{BigEndian, WriteBytesExt};
 use tokio;
 use tokio::prelude::*;
 
 use crate::Zookeeper;
 use futures::StartSend;
 
-struct Packetizer<S>{
+#[repr(i32)]
+#[allow(dead_code)]
+pub(super) enum OpCode {
+    Notification = 0,
+    Create = 1,
+    Delete = 2,
+    Exists = 3,
+    GetData = 4,
+    SetData = 5,
+    GetACL = 6,
+    SetACL = 7,
+    GetChildren = 8,
+    Synchronize = 9,
+    Ping = 11,
+    GetChildren2 = 12,
+    Check = 13,
+    Multi = 14,
+    Auth = 100,
+    SetWatches = 101,
+    Sasl = 102,
+    CreateSession = -10,
+    CloseSession = -11,
+    Error = -1,
+}
+
+pub(crate) struct Packetizer<S> {
     stream: S,
+
+    /// bytes we have not yet set
     outbox: Vec<u8>,
+
+    /// prefix of outbox that has been sent
     outstart: usize,
+
+    /// bytes we have not yet deserialized
     inbox: Vec<u8>,
+
+    /// prefix of inbox that has been sent
     instart: usize,
+    xid: i32,
 }
 
-fn wrap<S>(stream: S)->Packetizer<S>
-    where S: AsyncRead + AsyncWrite
-{
-    Packetizer{ stream, outbox: vec![], outstart: 0, inbox: vec![], instart: 0 }
+impl<S> Packetizer<S> {
+    pub(crate) fn new(stream: S) -> Packetizer<S> {
+        Packetizer {
+            stream,
+            outbox: vec![],
+            outstart: 0,
+            inbox: vec![],
+            instart: 0,
+            xid: 0,
+        }
+    }
 }
 
-enum ZookeeperRequest{
-    ConnectRequest{
+pub(crate) enum ZookeeperRequest {
+    Connect {
         protocol_version: i32,
         last_zxid_seen: i64,
         timeout: i32,
         session_id: i64,
         passwd: Vec<u8>,
         read_only: bool,
+    },
+}
+
+impl ZookeeperRequest {
+    fn serialize_into(&self, buffer: &mut Vec<u8>) -> Result<usize, io::Error> {
+        match self {
+            ZookeeperRequest::Connect {
+                protocol_version,
+                last_zxid_seen,
+                timeout,
+                session_id,
+                passwd,
+                read_only,
+            } => {
+                let start = buffer.len();
+                buffer.write_i32::<BigEndian>(*protocol_version);
+                buffer.write_i64::<BigEndian>(*last_zxid_seen);
+                buffer.write_i32::<BigEndian>(*timeout);
+                buffer.write_i64::<BigEndian>(*session_id);
+                buffer.write_i32::<BigEndian>(passwd.len() as i32);
+                buffer.write_all(&*passwd);
+                buffer.write_u8(read_only as u8);
+                Ok(buffer.len() - start)
+            }
+        }
     }
 }
 
-
-
-impl <S> Sink for Packetizer<S> where S: AsyncWrite{
+impl<S> Sink for Packetizer<S>
+where
+    S: AsyncWrite,
+{
     type SinkItem = ZookeeperRequest;
     type SinkError = failure::Error;
 
     fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
-        let type_and_payload = item.serialize();
-        let xid = self.xid;
-        let length = type_and_payload.length() + 4;
-        self.outbox.push(length);
-        self.outbox.push(xid);
-        self.outbox.append(type_and_payload);
+        let lengthi = self.outbox.len();
+        self.outbox.push(0);
+        self.outbox.push(0);
+        self.outbox.push(0);
+        self.outbox.push(0);
+
+        if let ZookeeperRequest::Connect { .. } = item {
+        } else {
+            self.outbox
+                .write_i32::<BigEndian>(self.xid)
+                .expect("vec:: should never fail");
+        };
+
+        let written = self.outbox.len() - lengthi - 4;
+
+        item.serialize_into(&mut self.outbox)
+            .expect("write should never fail");
+
+        let length = &mut self.outbox[lengthi..lengthi + 4];
+        length
+            .write_i32::<BigEndian>(written as i32)
+            .expect("vec should never fail");
         Ok(AsyncSink::Ready)
     }
 
     fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
         let n = try_ready!(self.stream.write(&self.outbox[self.outstart..]));
-        if self.outstart == self.outbox.len(){
+        if self.outstart == self.outbox.len() {
             self.outbox.clear();
             self.outstart = 0;
             Ok(Async::Ready(()))
-        }else{
+        } else {
             Ok(Async::NotReady)
         }
     }
